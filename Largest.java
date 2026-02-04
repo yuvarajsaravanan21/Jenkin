6765bfbeef58ceafa63e7f6e174b9335680adf3e
yuvaraj S <yuvaraj.saravanan2023@vitstudent.ac.in>
2026-02-04 21:02:52 +0530

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -4,9 +4,9 @@ public class Largest {
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
         int min = 1;
-        int max = 50;
+        int max = 5000;
         
-        int a = 23;
+        int a = 2300;
         
         int b = 34;
         
